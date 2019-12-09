ff8a83bd626b55beb31c1067f2b749f2ace65b95
jacksonwjc99 <weijie11@hotmail.com.tw>
2019-12-09 23:46:28 +0800

    commit

--- app/src/main/java/com/example/carloan/MainActivity.kt
+++ app/src/main/java/com/example/carloan/MainActivity.kt
@@ -19,25 +19,42 @@ class MainActivity : AppCompatActivity() {
             calcCarLoan(it)
         }
 
-        findViewById<Button>(R.id.buttonCalculate).setOnClickListener {
-            calcCarInterest(it)
+        findViewById<Button>(R.id.buttonReset).setOnClickListener {
+            reset(it)
         }
     }
 
     private fun calcCarLoan(view: View){
         val carPrice = findViewById<EditText>(R.id.editTextCarPrice).text.toString().toInt()
         val downPayment = findViewById<EditText>(R.id.editTextDownPayment).text.toString().toInt()
-
-        val string: String = getString(R.string.loan)
-        textViewLoan.text = string + " RM" +(carPrice - downPayment).toString();
-    }
-
-    private fun calcCarInterest(view: View){
-        val carPrice = findViewById<EditText>(R.id.editTextCarPrice).text.toString().toInt()
-        val carInterest = findViewById<EditText>(R.id.editTextInterestRate).text.toString().toInt()
+        val carInterest = findViewById<EditText>(R.id.editTextInterestRate).text.toString().toDouble()
         val intPeriod = findViewById<EditText>(R.id.editTextLoanPeriod).text.toString().toInt()
 
-        val string: String = getString(R.string.interest)
-        textViewInterest.text = string + " RM" +(carPrice * carInterest * intPeriod).toString();
+        val carLoan = carPrice - downPayment;
+        val interest = (carPrice * carInterest) * intPeriod;
+        val monthlyRepay = ((carLoan + interest) / intPeriod) / 12;
+
+        val string: String = getString(R.string.loan)
+        val string1: String = getString(R.string.interest)
+        val string2: String = getString(R.string.monthly_repayment)
+
+        textViewLoan.text = string + " RM" + carLoan;
+        textViewInterest.text = string1 + " RM" + interest;
+        textViewMonthlyRepayment.text = string2 + " RM" + monthlyRepay;
+    }
+
+    private fun reset(view: View){
+        findViewById<EditText>(R.id.editTextCarPrice).text = null;
+        findViewById<EditText>(R.id.editTextDownPayment).text = null;
+        findViewById<EditText>(R.id.editTextInterestRate).text = null;
+        findViewById<EditText>(R.id.editTextLoanPeriod).text = null;
+
+        val string: String = getString(R.string.loan)
+        val string1: String = getString(R.string.interest)
+        val string2: String = getString(R.string.monthly_repayment)
+
+        textViewLoan.text = string;
+        textViewInterest.text = string1;
+        textViewMonthlyRepayment.text = string2;
     }
 }
